d3d6fd2ed85ea7ddfa1a31d835e32ef6c90f80de
hp <hp@LAPTOP-OKPP4GNV>
2022-10-02 10:38:13 +0530

    adding new Test....

---
+++ BDD22OP/src/test/java/com/demo/DemoTest2.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	public class DemoTest {
+		
+		
+		@Test
+		  public void demoTest() {
+			System.out.println("Hello World...");
+		}
+
+	}
+}
